015c446ba8956d60aa9a0665b203a45dcaa51bb1
Wojciech Muszala <wojciech.muszala@gmail.com>
2025-06-05 12:44:48 +0200

    feat(result): remove dummy data, use calc fnct

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,12 @@
-const Results = () => {
+import { calculateEarnings, format } from "../utils/calculateEarnings";
+
+const Results = ({ userData }) => {
+  const noData = !userData || Object.keys(userData).length === 0;
+  const results = noData ? {} : calculateEarnings(userData);
+
+  console.log("userData:", userData);
+  console.log("Results:", results);
+
   return (
     <div className='w-full px-4 py-2 bg-background border border-primary-light rounded text-white'>
       <h2 className='text-lg font-semibold mb-4'>Podsumowanie finansowe</h2>
@@ -6,39 +14,57 @@ const Results = () => {
         <tbody className='divide-y divide-primary-light/20'>
           <tr>
             <td className='py-2'>Kwota brutto</td>
-            <td className='py-2 text-right'>12 300 zł</td>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.grossIncome)}
+            </td>
           </tr>
           <tr>
             <td className='py-2'>VAT (23%)</td>
-            <td className='py-2 text-right'>-2 300 zł</td>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.vat)}
+            </td>
           </tr>
           <tr>
             <td className='py-2'>Kwota netto przed kosztami</td>
-            <td className='py-2 text-right'>10 000 zł</td>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.netIncomeBeforeCosts)}
+            </td>
           </tr>
-          <tr>
+          {/* <tr>
             <td className='py-2'>Koszty uzyskania</td>
-            <td className='py-2 text-right'>-1 200 zł</td>
-          </tr>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.expenses)}
+            </td>
+          </tr> */}
           <tr>
             <td className='py-2'>ZUS (całkowity)</td>
-            <td className='py-2 text-right'>-1 400 zł</td>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.socialSecurity)}
+            </td>
           </tr>
           <tr>
             <td className='py-2'>Podatek dochodowy</td>
-            <td className='py-2 text-right'>-900 zł</td>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.incomeTax)}
+            </td>
           </tr>
           <tr className='font-semibold text-primary-light'>
             <td className='py-2'>Dochód na rękę</td>
-            <td className='py-2 text-right'>6 500 zł</td>
+            <td className='py-2 text-right'>
+              {noData ? "-" : format(results.netIncome)}
+            </td>
           </tr>
           <tr className='text-xs text-text/70'>
             <td className='pt-4'>Efektywna dniówka</td>
-            <td className='pt-4 text-right'>325 zł / dzień</td>
+            <td className='pt-4 text-right'>
+              {noData ? "-" : format(results.netIncome / 21)}
+            </td>
           </tr>
           <tr className='text-xs text-text/70'>
             <td className='py-1'>Efektywna stawka godzinowa</td>
-            <td className='py-1 text-right'>40,63 zł / godz.</td>
+            <td className='py-1 text-right'>
+              {noData ? "-" : format(results.netIncome / 168)}
+            </td>
           </tr>
         </tbody>
       </table>
